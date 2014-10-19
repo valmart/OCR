8324adaafc44e8b59f7a57900115c358c8956717
mondolq <mondolq.beadeveloper@gmail.com>
2014-10-19 14:09:46 +0800

    TOTO

--- toto.cpp
+++ toto.cpp
@@ -1,63 +1,32 @@
+#include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "iostream"
 using namespace cv;
 using namespace std;
-
-Mat src; Mat src_gray;
-int thresh = 100;
-int max_thresh = 255;
-RNG rng(12345);
-
-/// Function header
-void thresh_callback(int, void* );
-
-/** @function main */
-int main( int argc, char** argv )
+int main( int ac, char **av)
 {
-  /// Load source image and convert it to gray
-  src = imread( argv[1], 1 );
-
-  /// Convert image to gray and blur it
-  cvtColor( src, src_gray, CV_BGR2GRAY );
-  blur( src_gray, src_gray, Size(3,3) );
-
-  /// Create Window
-  char* source_window = "Source";
-  namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-  imshow( source_window, src );
-
-  createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
-
-  waitKey(0);
-  return(0);
-}
-
-/** @function thresh_callback */
-void thresh_callback(int, void* )
-{
-  Mat canny_output;
+  Mat image;
+  image = imread(av[1], 1);  
+  namedWindow( "Display window", CV_WINDOW_AUTOSIZE );  
+  imshow( "Display window", image );
+  Mat gray;
+  cvtColor(image, gray, CV_BGR2GRAY);
+  Canny(gray, gray, 100, 200, 3);
+  /// Find contours   
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
-
-  /// Detect edges using canny
-  Canny( src_gray, canny_output, thresh, thresh*2, 3 );
-  /// Find contours
-  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
+  RNG rng(12345);
+  findContours( gray, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
   /// Draw contours
-  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+  Mat drawing = Mat::zeros( gray.size(), CV_8UC3 );
   for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-     }
-
-  /// Show in a window
-  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  imshow( "Contours", drawing );
+    {
+      Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+      drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    }     
+ 
+  imshow( "Result window", drawing );
+  waitKey(0);                                         
+  return 0;
 }
